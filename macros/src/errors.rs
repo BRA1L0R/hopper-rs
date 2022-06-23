d97ee742357a3643d1f38e5081556b16d44d9608
DumbMahreeo <dumbmahreeo@github.com>
2022-06-23 22:41:22 +0200

    Refactor of hopper_macros + Derive(Serialize) + minor version bump

---
+++ macros/src/errors.rs
@@ -0,0 +1,17 @@
+use std::fmt::Display;
+
+/// Errors used in hopper_macros.
+#[derive(Debug)]
+pub enum Error {
+    /// Used when an invalid struct (enum or with unnamed fields) gets parsed.
+    InvalidStructErr,
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("Invalid Struct Error (enum or unnamed fields)");
+        Ok(())
+    }
+}
+
+impl std::error::Error for Error {}
